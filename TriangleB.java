f4782f2c1e5b785377e05d365d6d94f86ae7f2b2
dev0xeb <clintonayomide96@gmail.com>
2024-06-23 14:16:09 +0100

    Add TriangleB.java file

---
+++ TriangleB.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class TriangleB
+{
+	public static void main(String[] args)
+	{
+	Scanner input = new Scanner(System.in);
+	
+	for (int pattern = 10; pattern >=1; pattern--){
+		for(int counter = 1; counter < pattern; counter++){
+		System.out.print("* ");}
+	System.out.println(' ');}
+	}
+}
