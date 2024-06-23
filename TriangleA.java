a79ccc1cb1c01b4510afd17a36d69d4202226367
dev0xeb <clintonayomide96@gmail.com>
2024-06-23 14:11:28 +0100

    Add TriangleA.java file

---
+++ TriangleA.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class TriangleA
+{
+	public static void main(String[] args)
+	{
+	Scanner input = new Scanner(System.in);
+
+	for (int pattern = 1; pattern <=10; pattern++){
+		for (int counter = 1; counter <= pattern; counter++){
+		System.out.print("* ");}
+	System.out.println(' ');}
+	}
+}
