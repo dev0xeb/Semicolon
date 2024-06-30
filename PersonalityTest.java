90a5500df2901f9012b378ced9e9d7f056ab28e2
dev0xeb <clintonayomide96@gmail.com>
2024-06-30 23:31:47 +0100

    Update PersonalityTest.java

--- PersonalityTest.java
+++ PersonalityTest.java
@@ -20,46 +20,46 @@ public class PersonalityTest
 	int Introvert= 0;
 
 	System.out.println("Do you prefer to focus on the outer world");
-	System.out.println("Enter 1 for yes or 0 for no");
-	int result = input.nextInt();
+	System.out.println("Enter A for yes or B for no");
+	char result = input.next().charAt(0);
 
-	if (result == 1){
+	if (result == 'A'){
 	Extrovert++;
 	}else {
 	Introvert++;}
 	
 	System.out.println("Do you gain energy from being around people");
-	System.out.println("Enter 1 for yes or 0 for no");
-	result = input.nextInt();
+	System.out.println("Enter A for yes or B for no");
+	result = input.next().charAt(0);
 
-	if (result == 1){
+	if (result == 'A'){
 	Extrovert++;
 	}else {
 	Introvert++;}
 
 	System.out.println("I prefer to work on projects myself");
-	System.out.println("Enter 1 for yes or 0 for no");
-	result = input.nextInt();
+	System.out.println("Enter A for yes or B for no");
+	result = input.next().charAt(0);
 
-	if (result == 1){
+	if (result == 'A'){
 	Extrovert++;
 	}else {
 	Introvert++;}
 
 	System.out.println("I find it easy to start conversations with strangers");
-	System.out.println("Enter 1 for yes or 0 for no");
-	result = input.nextInt();
+	System.out.println("Enter A for yes or B for no");
+	result = input.next().charAt(0);
 
-	if (result == 1){
+	if (result == 'A'){
 	Extrovert++;
 	}else {
 	Introvert++;}
 
 	System.out.println("I enjoy being the center of attention");
-	System.out.println("Enter 1 for yes or 0 for no");
-	result = input.nextInt();
+	System.out.println("Enter A for yes or B for no");
+	result = input.next().charAt(0);
 
-	if (result == 1){
+	if (result == 'A'){
 	Extrovert++;
 	}else {
 	Introvert++;}
@@ -72,46 +72,46 @@ public class PersonalityTest
 	int Intuition = 0;
 
 	System.out.println("I prefer pratical and realistic information over abstract ideas");
-	System.out.println("Enter 1 for yes or 0 for no");
-	result = input.nextInt();
+	System.out.println("Enter A for yes or B for no");
+	result = input.next().charAt(0);
 
-	if (result == 1){
+	if (result == 'A'){
 	Sensing++;
 	}else {
 	Intuition++;}
 
 	System.out.println("I focus on future possibilities rather than past experiences");
-	System.out.println("Enter 1 for yes or 0 for no");
-	result = input.nextInt();
+	System.out.println("Enter A for yes or B for no");
+	result = input.next().charAt(0);
 
-	if (result == 1){
+	if (result == 'A'){
 	Sensing++;
 	}else {
 	Intuition++;}
 
 	System.out.println("I trust my hunches and gut feelings");
-	System.out.println("Enter 1 for yes or 0 for no");
-	result = input.nextInt();
+	System.out.println("Enter A for yes or B for no");
+	result = input.next().charAt(0);
 
-	if (result == 1){
+	if (result == 'A'){
 	Sensing++;
 	}else {
 	Intuition++;}
 
 	System.out.println("I am detail oriented and focus on the specifics");
-	System.out.println("Enter 1 for yes or 0 for no");
-	result = input.nextInt();
+	System.out.println("Enter A for yes or B for no");
+	result = input.next().charAt(0);
 
-	if (result == 1){
+	if (result == 'A'){
 	Sensing++;
 	}else {
 	Intuition++;}
 
 	System.out.println("I enjoy brainstorming and thinking about the big picture");
-	System.out.println("Enter 1 for yes or 0 for no");
-	result = input.nextInt();
+	System.out.println("Enter A for yes or B for no");
+	result = input.next().charAt(0);
 
-	if (result == 1){
+	if (result == 'A'){
 	Sensing++;
 	}else {
 	Intuition++;}
@@ -123,46 +123,46 @@ public class PersonalityTest
 	int Feeling = 0;
 
 	System.out.println("i make decisions based on logic and objective criteria");
-	System.out.println("Enter 1 for yes or 0 for no");
-	result = input.nextInt();
+	System.out.println("Enter A for yes or B for no");
+	result = input.next().charAt(0);
 
-	if (result == 1){
+	if (result == 'A'){
 	Thinking++;
 	}else {
 	Feeling++;}
 
 	System.out.println("I consider the impact of my decisions on others feelings");
-	System.out.println("Enter 1 for yes or 0 for no");
-	result = input.nextInt();
+	System.out.println("Enter A for yes or B for no");
+	result = input.next().charAt(0);
 
-	if (result == 1){
+	if (result == 'A'){
 	Thinking++;
 	}else {
 	Feeling++;}
 
 	System.out.println("I value fairness and justice over personal considerations");
-	System.out.println("Enter 1 for yes or 0 for no");
-	result = input.nextInt();
+	System.out.println("Enter A for yes or B for no");
+	result = input.next().charAt(0);
 
-	if (result == 1){
+	if (result == 'A'){
 	Thinking++;
 	}else {
 	Feeling++;}
 
 	System.out.println("I prioritize harmony over conflict");
-	System.out.println("Enter 1 for yes or 0 for no");
-	result = input.nextInt();
+	System.out.println("Enter A for yes or B for no");
+	result = input.next().charAt(0);
 
-	if (result == 1){
+	if (result == 'A'){
 	Thinking++;
 	}else {
 	Feeling++;}
 
 	System.out.println("I am more concerned with being tactful than being truthful");
-	System.out.println("Enter 1 for yes or 0 for no");
-	result = input.nextInt();
+	System.out.println("Enter A for yes or B for no");
+	result = input.next().charAt(0);
 
-	if (result == 1){
+	if (result == 'A'){
 	Thinking++;
 	}else {
 	Feeling++;}
@@ -175,46 +175,46 @@ public class PersonalityTest
 	int Perceiving = 0;
 
 	System.out.println("i like to have a plan and stick to it");
-	System.out.println("Enter 1 for yes or 0 for no");
-	result = input.nextInt();
+	System.out.println("Enter A for yes or B for no");
+	result = input.next().charAt(0);
 
-	if (result == 1){
+	if (result == 'A'){
 	Judging++;
 	}else {
 	Perceiving++;}
 
 	System.out.println("i prefer to keep my options open and be spontaneous");
-	System.out.println("Enter 1 for yes or 0 for no");
-	result = input.nextInt();
+	System.out.println("Enter A for yes or B for no");
+	result = input.next().charAt(0);
 
-	if (result == 1){
+	if (result == 'A'){
 	Judging++;
 	}else {
 	Perceiving++;}
 
 	System.out.println("i feel stressed when things are unplanned or last minute");
-	System.out.println("Enter 1 for yes or 0 for no");
-	result = input.nextInt();
+	System.out.println("Enter A for yes or B for no");
+	result = input.next().charAt(0);
 
-	if (result == 1){
+	if (result == 'A'){
 	Judging++;
 	}else {
 	Perceiving++;}
 
 	System.out.println("i am comfortable with changing plans if needed");
-	System.out.println("Enter 1 for yes or 0 for no");
-	result = input.nextInt();
+	System.out.println("Enter A for yes or B for no");
+	result = input.next().charAt(0);
 
-	if (result == 1){
+	if (result == 'A'){
 	Judging++;
 	}else {
 	Perceiving++;}
 
 	System.out.println("i prefer to make decisions early rather than waiting until the last minute");
-	System.out.println("Enter 1 for yes or 0 for no");
-	result = input.nextInt();
+	System.out.println("Enter A for yes or B for no");
+	result = input.next().charAt(0);
 
-	if (result == 1){
+	if (result == 'A'){
 	Judging++;
 	}else {
 	Perceiving++;}
@@ -233,7 +233,7 @@ public class PersonalityTest
 	System.out.println("Judging: " + Judging);
 	System.out.println("Perceiving: " + Perceiving);
 	
-	System.out.print(name + ", you MBTI test result is ");
+	System.out.print(name + ", your MBTI test result is ");
 	if (Extrovert > Introvert){
 	System.out.print("E");
 	}else{
