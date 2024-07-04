29768661673804466a6a7df8d3ce22aa9e578f30
dev0xeb <clintonayomide96@gmail.com>
2024-07-04 21:25:01 +0100

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Task1{
+public static void main(String[] args){
+  Scanner input = new Scanner(System.in);
+    
+   
+    int sum = 0;
+    
+	for(int count=1; count <= 10; count++){
+	System.out.println("Enter scores: ");
+	int scores = input.nextInt();
+	
+	sum = sum + scores;}
+	System.out.println(sum);
+	}
+}
