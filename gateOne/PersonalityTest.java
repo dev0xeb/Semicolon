648185324004cc968e162a207f92b07815b519eb
dev0xeb <clintonayomide96@gmail.com>
2024-08-05 21:28:26 +0100

    <phase gate file>

---
+++ gateOne/PersonalityTest.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PersonalityTest {
+	public static void main(String[] args) [
+	Scanner input = new Scanner(System.in);
+
+	System.out.println("Kindly enter your name to take the MBTI Personality Test: ");
+	String userName = input.nextLine();
+
+	System.out.println("Welcome " + userName + ", You can now take the test")
+
+
+	String[] personalityQuestions = {"Do you prefer to focus on the outer world, Enter A for Yes or B for No.",
+					"Do you gain energy from being around people, Enter A for Yes or B for No.",
+					"I prefer to work on projects myself. Enter A for Yes or B for No."
+					"I find it easy to start conversations with strangers. Enter A for Yes or B for No.",
+					"I enjoy being the center of attention. Enter A for Yes or B for No.",
+					"I prefer pratical and realistic information over abstract ideas. Enter A for Yes or B for No.",
+					"I focus on future possibilities rather than past experiences. Enter A for Yes or B for No.",
+					"I trust my hunches and gut feelings. Enter A for Yes or B for No.",
+					"I am detail oriented and focus on the specifics. Enter A for Yes or B for No.",
+					"I enjoy brainstorming and thinking about the big picture. Enter A for Yes or B for No.",
+					"i make decisions based on logic and objective criteria. Enter A for Yes or B for No.",
+					"I consider the impact of my decisions on others feelings. Enter A for Yes or B for No.",
+					"I value fairness and justice over personal considerations. Enter A for Yes or B for No.",
+					"I prioritize harmony over conflict. Enter A for Yes or B for No.",
+					"I am more concerned with being tactful than being truthful. Enter A for Yes or B for No.",
+					"i like to have a plan and stick to it. Enter A for Yes or B for No.",
+					"i prefer to keep my options open and be spontaneous. Enter A for Yes or B for No.",
+					"i feel stressed when things are unplanned or last minute. Enter A for Yes or B for No.",
+					"i am comfortable with changing plans if needed. Enter A for Yes or B for No.",
+					"i prefer to make decisions early rather than waiting until the last minute. Enter A for Yes or B for No.",
+					}
