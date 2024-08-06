882444539e3e52dd03ef4c53cec52094c9f6d2b1
dev0xeb <clintonayomide96@gmail.com>
2024-08-06 20:53:30 +0100

    phase gate file

--- gateOne/PersonalityTest.java
+++ gateOne/PersonalityTest.java
@@ -2,33 +2,207 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class PersonalityTest {
-	public static void main(String[] args) [
+	public static void main(String[] args) {
 	Scanner input = new Scanner(System.in);
 
 	System.out.println("Kindly enter your name to take the MBTI Personality Test: ");
 	String userName = input.nextLine();
 
-	System.out.println("Welcome " + userName + ", You can now take the test")
+	System.out.println("Welcome " + userName + ", You can now take the test");
 
-
-	String[] personalityQuestions = {"Do you prefer to focus on the outer world, Enter A for Yes or B for No.",
+	System.out.println();
+	String[] personalityQuestions = { "Introvert/Extrovert.\n Do you prefer to focus on the outer world, Enter A for Yes or B for No.",
 					"Do you gain energy from being around people, Enter A for Yes or B for No.",
-					"I prefer to work on projects myself. Enter A for Yes or B for No."
+					"I prefer to work on projects myself. Enter A for Yes or B for No.",
 					"I find it easy to start conversations with strangers. Enter A for Yes or B for No.",
 					"I enjoy being the center of attention. Enter A for Yes or B for No.",
-					"I prefer pratical and realistic information over abstract ideas. Enter A for Yes or B for No.",
+					"Sensing/Intuition.\nI prefer pratical and realistic information over abstract ideas. Enter A for Yes or B for No.",
 					"I focus on future possibilities rather than past experiences. Enter A for Yes or B for No.",
 					"I trust my hunches and gut feelings. Enter A for Yes or B for No.",
 					"I am detail oriented and focus on the specifics. Enter A for Yes or B for No.",
 					"I enjoy brainstorming and thinking about the big picture. Enter A for Yes or B for No.",
-					"i make decisions based on logic and objective criteria. Enter A for Yes or B for No.",
+					"Thinking/Feeling.\n I make decisions based on logic and objective criteria. Enter A for Yes or B for No.",
 					"I consider the impact of my decisions on others feelings. Enter A for Yes or B for No.",
 					"I value fairness and justice over personal considerations. Enter A for Yes or B for No.",
 					"I prioritize harmony over conflict. Enter A for Yes or B for No.",
 					"I am more concerned with being tactful than being truthful. Enter A for Yes or B for No.",
-					"i like to have a plan and stick to it. Enter A for Yes or B for No.",
+					"Judging/Perceiving.\n i like to have a plan and stick to it. Enter A for Yes or B for No.",
 					"i prefer to keep my options open and be spontaneous. Enter A for Yes or B for No.",
 					"i feel stressed when things are unplanned or last minute. Enter A for Yes or B for No.",
 					"i am comfortable with changing plans if needed. Enter A for Yes or B for No.",
-					"i prefer to make decisions early rather than waiting until the last minute. Enter A for Yes or B for No.",
-					}
+					"i prefer to make decisions early rather than waiting until the last minute. Enter A for Yes or B for No."
+					};
+	int Introvert = 0;
+	int Extrovert = 0;
+	int Sensing = 0;
+	int Intuition = 0;
+	int Thinking = 0;
+	int Feeling = 0;
+	int Judging = 0;
+	int Perceiving = 0;
+
+	for(int displayQuestions = 0; displayQuestions < personalityQuestions.length; displayQuestions++) {
+	System.out.print(personalityQuestions[displayQuestions]);
+	System.out.println("Enter A/B");
+	String userInput = input.nextLine();
+	String newUserInput = userInput.toUpperCase();
+	
+		if (newUserInput == "A" || newUserInput == "B"){
+		break;
+			}
+
+	switch(displayQuestions) {
+		case 0:
+			if (userInput == "A")
+				Extrovert++;
+			else if (userInput == "B")
+				Introvert++;
+			break;
+		case 1:
+			if (userInput == "A")
+				Extrovert++;
+			else if (userInput == "B")
+				Introvert++;
+			break;
+		case 2:
+			if (userInput == "A")
+				Extrovert++;
+			else if (userInput == "B")
+				Introvert++;
+			break;
+		case 3:
+			if (userInput == "A")
+				Extrovert++;
+			else if (userInput == "B")
+				Introvert++;
+			break;
+		case 4:
+			if (userInput == "A")
+				Extrovert++;
+			else if (userInput == "B")
+				Introvert++;
+			break;
+		case 5:
+			if (userInput == "A")
+				Sensing++;
+			else if (userInput == "B")
+				Intuition++;
+			break;
+		case 6:
+			if (userInput == "A")
+				Sensing++;
+			else if (userInput == "B")
+				Intuition++;
+			break;
+		case 7:
+			if (userInput == "A")
+				Sensing++;
+			else if (userInput == "B")
+				Intuition++;
+			break;
+		case 8:
+			if (userInput == "A")
+				Sensing++;
+			else if (userInput == "B")
+				Intuition++;
+			break;
+		case 9:
+			if (userInput == "A")
+				Sensing++;
+			else if (userInput == "B")
+				Intuition++;
+			break;
+		case 10:
+			if (userInput == "A")
+				Thinking++;
+			else if(userInput == "B")
+				Feeling++;
+			break;
+		case 11:
+			if (userInput == "A")
+				Thinking++;
+			else if(userInput == "B")
+				Feeling++;
+			break;
+		case 12:
+			if (userInput == "A")
+				Thinking++;
+			else if(userInput == "B")
+				Feeling++;
+			break;
+		case 13:if (userInput == "A")
+				Thinking++;
+			else if(userInput == "B")
+				Feeling++;
+			break;
+		case 14:
+			if (userInput == "A")
+				Thinking++;
+			else if(userInput == "B")
+				Feeling++;
+			break;
+		case 15:
+			if (userInput == "A")
+				Judging++;
+			else if (userInput == "B")
+				Perceiving++;
+			break;
+		case 16:
+			if (userInput == "A")
+				Judging++;
+			else if (userInput == "B")
+				Perceiving++;
+			break;
+		case 17:
+			if (userInput == "A")
+				Judging++;
+			else if (userInput == "B")
+				Perceiving++;
+			break;
+		case 18:
+			if (userInput == "A")
+				Judging++;
+			else if (userInput == "B")
+				Perceiving++;
+			break;
+		case 19:
+			if (userInput == "A")
+				Judging++;
+			else if (userInput == "B")
+				Perceiving++;
+			break;
+		default:
+			System.out.println("Input mismatch");
+
+			}
+		}
+
+		/*System.out.println("Your MBTI personality Test result is: ");
+		if (Extrovert > Introvert){
+		System.out.print("E");
+		}else{
+		System.out.print("I");}
+
+		if (Sensing > Intuition){
+		System.out.print("S");
+		}else{
+		System.out.print("I");}
+
+		if (Thinking > Feeling){
+		System.out.print("T");
+		}else{
+		System.out.print("F");}
+
+		if (Judging > Perceiving){
+		System.out.print("J");
+		}else{
+		System.out.print("P");}*/
+
+		System.out.println("Your MBTI personality Test result is: ");
+        	System.out.print((Extrovert > Introvert ? "E" : "I"));
+       		System.out.print((Sensing > Intuition ? "S" : "N"));
+        	System.out.print((Thinking > Feeling ? "T" : "F"));
+        	System.out.print((Judging > Perceiving ? "J" : "P"));
+	}
+}
+		
--- gateOne/StudentGradeArray.java
+++ gateOne/StudentGradeArray.java
@@ -14,35 +14,40 @@ int numberOfSubjects = input.nextInt();
 System.out.println("Saving >>>>>>>>>>>>>>>>");
 System.out.println("Saved successfully");
 
-int[][] scores = new int[numberOfStudents][numberOfSubjects];
-int[] studentsum = new int[numberOfStudents];
-double[] studentaverage = new double[numberOfStudents];
+int[][] scores = new int[numberOfStudents][numberOfSubjects]; //holds scores for each student and subject. holding the size of the number of students and number of subjects. 
+int[] studentsum = new int[numberOfStudents]; //storing the sum
+double[] studentaverage = new double[numberOfStudents]; //storing the average for ths scores
 int sum = 0;
 
+//iterating the inputs for each students
+//iterates from student variable from 0 to the number of students length.
 for (int student = 0; student < numberOfStudents; student++) {
 	System.out.println();
 	System.out.println("Entering scores for Student " + (student + 1));
-	for(int studentScores = 0; studentScores < numberOfSubjects; studentScores++) {
-	System.out.println("Enter the student score in subject " + (studentScores + 1));
-	int score = input.nextInt();
-	System.out.println("Saving >>>>>>>>>>>>>>>>");
-	System.out.println("Saved successfully");
-		if(score > 100 || score < 0){
-		System.out.println("Invalid score, Enter the student score in subject " + (studentScores + 1));
-		score= input.nextInt();
+		//iterate through each subject
+		for(int studentScores = 0; studentScores < numberOfSubjects; studentScores++) {
+			System.out.println();
+			System.out.println("Enter the student score in subject " + (studentScores + 1));
+			int score = input.nextInt();
+			System.out.println("Saving >>>>>>>>>>>>>>>>");
+			System.out.println("Saved successfully");
+				if(score > 100 || score < 0){
+				System.out.println("Invalid score, Enter the student score in subject " + (studentScores + 1));
+				score= input.nextInt();
 	
-		}
+				}
 	   
-		scores[student][studentScores] = score;
-		studentsum[student] += score;
-	}
+			scores[student][studentScores] = score;
+			studentsum[student] += score;
+		}
 
 	
 	studentaverage[student] = (double) studentsum[student] / numberOfSubjects;
 }
+//Printing each students summary
 System.out.println();
 for (int student = 0; student < numberOfStudents; student++) {
-System.out.println("The summary of the performance for Student " + (student + 1) + " is:");
+System.out.println("The summary of the performance for Student " + (student + 1) + " is ");
 for(int studentScores = 0; studentScores < numberOfSubjects; studentScores++) {
 System.out.print(scores[student][studentScores] + " ");
 }
@@ -52,11 +57,20 @@ System.out.printf("the average Score for Student %d is %.2f\n", (student + 1), s
 System.out.println();
 }
 
+//Printing the table
 System.out.printf("STUDENT\t");
 for(int subject = 0; subject < numberOfSubjects; subject++){
 	System.out.printf("\tSUB" + (subject + 1));
 	}
-System.out.print("\tTOTAL\tAVERAGE");
+System.out.print("\tTOTAL\tAVERAGE\n");
+
+for(int studentScores = 0; studentScores < numberOfStudents; studentScores++) {
+System.out.printf("Student %d\t", studentScores + 1);
+	for(int subject =0; subject < numberOfSubjects; subject++) {
+	System.out.printf("%d\t", scores[studentScores][subject]);
+	}
+System.out.printf("%d\t%.2f\n", studentsum[studentScores], studentaverage[studentScores]);
+}
 
 
 
