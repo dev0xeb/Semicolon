85f4e818d24401dcbbd7c5d32e6b8475708a0c79
dev0xeb <clintonayomide96@gmail.com>
2024-08-07 23:59:36 +0100

    phase gate

--- gateOne/CreditCardValidity.java
+++ gateOne/CreditCardValidity.java
@@ -5,45 +5,58 @@ public class CreditCardValidity {
 	Scanner input = new Scanner(System.in);
 	
 	int additonRightToLeft = 0;
+	int oddTotal = 0;
 
 	System.out.println("Welcome to credit card validator where you can get all information regarding your CreditCard");
 	System.out.println("Enter your Credit Card number ");
 	String cardNumber = input.next();
 
+	System.out.println();
+	System.out.println("*********************************************************");
 	if (cardNumber.length() < 13 || cardNumber.length() > 16){
-	System.out.print("Invalid Card Number. Card Number should be between a 13 digit to a 16 digit number, Try again: ");
+	System.out.print("Invalid Card Number.");
 	} else{
 	if (cardNumber.charAt(0) == '4'){
-	System.out.println("Your Card is Visa Card");}
+	System.out.println("**Credit Card type: Visa Card");}
 	
 	else if (cardNumber.charAt(0) == '5'){
-	System.out.println("Your Card is a Master Card");}
+	System.out.println("**Credit Card type: Master Card");}
 
 	else if (cardNumber.charAt(0) == '3' && cardNumber.charAt(1) == '7'){
-	System.out.println("Your Card is an American Express Card");}
+	System.out.println("**Credit Card type: American Express Card");}
 
 	else if (cardNumber.charAt(0) == '6'){
-	System.out.println("Your Card is a Discover Card");}
+	System.out.println("**Credit Card type: Discover Card");}
 
 	else{
-	System.out.println("Invalid");
+	System.out.println("Invalid Card");
 	}
 	}
 
 	for(int rightToLeft = cardNumber.length()-2; rightToLeft >= 0; rightToLeft-=2) {
-		int number = cardNumber.charAt(rightToLeft);
-		int doubleDigit = number * 2;
-		if(doubleDigit > 9){
-		int num1 = doubleDigit / 10;
-		int num2 = doubleDigit % 10;
+		int number = Integer.parseInt(String.valueOf(cardNumber.charAt(rightToLeft)));
+		number = number * 2;
+		if(number > 9)
+		number = number / 10 + number % 10;
+
+		additonRightToLeft += number;}
+		//System.out.print(additonRightToLeft);
+
+	for(int rightToLeft = cardNumber.length()-1; rightToLeft >= 0; rightToLeft-=2) {
+		int numbers = Integer.parseInt(String.valueOf(cardNumber.charAt(rightToLeft)));
+		oddTotal += numbers;
+		}
+		//System.out.print(oddTotal);
+	
+	System.out.println("**Credit Card Number: " + cardNumber);
+	System.out.println("**Credit Card Digit Length: " + cardNumber.length());
+	int sum = additonRightToLeft + oddTotal;
+	if(sum % 10 == 0)
+	System.out.println("**Credit Card Validity Status: Valid");
+	else
+	System.out.println("**Credit Card Validity Status: Invalid");
 		
-		doubleDigit = num1 + num2;}
-
-		additonRightToLeft += doubleDigit;}
-	System.out.print(additonRightToLeft);
-
-
-		
+	System.out.println("*********************************************************");
 	
 	
 	
